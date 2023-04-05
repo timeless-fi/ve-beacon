e6bfa72a22812198a7a8aa2d3e873856c819b1ac
ZeframLou <zeframlou@gmail.com>
2023-04-04 17:45:51 -0700

    initial changes

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,21 +2,21 @@
 pragma solidity ^0.8.13;
 
 import {CREATE3Script} from "./base/CREATE3Script.sol";
-import {Contract} from "../src/Contract.sol";
+import {VeBeacon} from "../src/VeBeacon.sol";
 
 contract DeployScript is CREATE3Script {
     constructor() CREATE3Script(vm.envString("VERSION")) {}
 
-    function run() external returns (Contract c) {
+    function run() external returns (VeBeacon c) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
 
         uint256 param = 123;
 
         vm.startBroadcast(deployerPrivateKey);
 
-        c = Contract(
+        c = VeBeacon(
             create3.deploy(
-                getCreate3ContractSalt("Contract"), bytes.concat(type(Contract).creationCode, abi.encode(param))
+                getCreate3ContractSalt("VeBeacon"), bytes.concat(type(VeBeacon).creationCode, abi.encode(param))
             )
         );
 
